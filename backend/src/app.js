46dd06b39a2bf70317447d8327bb0c4f4083cc2d
Dibyani <sdibyani123@gmail.com>
2025-10-19 23:00:45 +0530

    Update backend URL and use environment variable for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,12 +14,23 @@ const app= express();
 const server= createServer(app);
 const io= connectToSocket(server);
 
+
+app.use(cors({
+  origin: ["https://apna-videocall-2.onrender.com"], // your frontend URL
+  credentials: true,
+}));
+
+
 app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb", extended:true}));
 
 app.use("/api/v1/users", userRoutes);
+
+
+
+
 
 const start= async()=>{
     app.set("mongo_user")
